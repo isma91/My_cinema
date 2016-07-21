3b2f5a80aa3ec74c84093002ee2b35c4986f8b49
isma91 <ismaydogmus@gmail.com>
2016-07-21 13:19:42 +0200

    redirect to the view

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Index.php
+*
+* Make a custom var_dump
+*
+* PHP 7.0.8-0ubuntu0.16.04.1 (cli) ( NTS )
+*
+* @category View
+* @package  View
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+header("Location: view/index.php");
